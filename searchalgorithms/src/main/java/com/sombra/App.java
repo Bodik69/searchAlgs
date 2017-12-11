56e948ccad9a70bc3b9e7bff146d71641d35094b
bodik69 <bsmachylo86@gmail.com>
2017-12-11 02:19:32 +0200

    added graph

--- searchalgorithms/src/main/java/com/sombra/App.java
+++ searchalgorithms/src/main/java/com/sombra/App.java
@@ -1,4 +1,7 @@
 package com.sombra;
+import com.sombra.algorithms.Dijkstra;
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.SingleGraph;
 
 /**
  * Hello world!
@@ -8,6 +11,34 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        int countOfNodes = 6;
+        Integer[][] graphData = new Integer[][] {
+                {0, 4, 2, null, null, null},
+                {null, 0, null, 5, null, null},
+                {null, 1, 0, null, 10, null},
+                {null, null, null, 0, 2, 6},
+                {null, null, null, null, 0, 3},
+                {null, null, null, null, null, 0}
+        };
+        Dijkstra dijkstra = new Dijkstra();
+        dijkstra.setNodeCount(countOfNodes);
+        dijkstra.setGraph(graphData);
+        dijkstra.calculate(0);
+        Graph graph = new SingleGraph("Dijkstra");
+
+        for (int i = 0; i < countOfNodes; i++) {
+            Node n = graph.addNode(String.valueOf(i));
+            n.addAttribute("ui.label", String.valueOf(i));
+        }
+        for (int i = 0; i < countOfNodes; i++) {
+            for (int j = 0; j < countOfNodes; j++) {
+                if (i != j && graphData[i][j] != null) {
+                    Edge edge = graph.addEdge(i + "_" + j, i, j);
+                    edge.addAttribute("ui.label", String.valueOf(graphData[i][j]));
+                }
+            }
+        }
+
+        graph.display();
     }
 }
